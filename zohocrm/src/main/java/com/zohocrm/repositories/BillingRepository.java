827cf725ab5fcb4f8fa4ffeb7978e61d8dbe567e
vinayakpadukone <109872673+vinayakpadukone@users.noreply.github.com>
2023-03-01 17:32:21 +0530

    Delete zohocrm directory

--- zohocrm/src/main/java/com/zohocrm/repositories/BillingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zohocrm.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zohocrm.entities.Billing;
-
-public interface BillingRepository extends JpaRepository<Billing, Long> {
-
-}
--- zohocrm/src/main/java/com/zohocrm/utility/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.zohocrm.utility;
-
-public interface EmailService {
-	 public void sendSimpleMessage(String to, String subject, String text);
-
-}
